be09740b26c08cc6002f32a09b20d63a6c386f10
wushaowei001 <jcwsw129@gmail.com>
2016-07-19 22:49:42 +0800

    update

--- index.js
+++ index.js
@@ -7,8 +7,34 @@ var window = document.defaultView;
 
 run();
 
+function getArrayFromFile(fileName) {
+	let content = fs.readFileSync('./input/' + fileName).toString().trim();
+	let list = content.split('\n').map(function (one) {
+			return one.trim();
+		});
+	console.log(fileName + ': ' + list.length);
+	return list;
+}
+
 function run() {
-	var content = fs.readFileSync('weike-s.txt').toString().trim();
+	var oxford3000_content = fs.readFileSync('./input/oxford3000.json').toString().trim();
+	var oxfordList = JSON.parse(oxford3000_content).map(function (obj) {
+			return obj.word;
+		});
+
+	var rankList = getArrayFromFile('20k.txt');
+
+	var cet4List = getArrayFromFile('cet4.txt');
+
+	var cet6List = getArrayFromFile('cet6.txt');
+
+	var tofelList = getArrayFromFile('tofel.txt');
+
+	var greList = getArrayFromFile('gre.txt');
+
+	//var content = fs.readFileSync('./input/test.txt').toString().trim();
+	var content = fs.readFileSync('./input/weike-input.txt').toString().trim();
+
 	var rows = content.split('\n');
 
 	var allObj = [];
@@ -21,6 +47,31 @@ function run() {
 
 		let word = div.childNodes[0].textContent.trim();
 		oneObj.word = word;
+
+		if (oxfordList.indexOf(word) > -1) {
+			oneObj.oxford3000 = true;
+		}
+
+		if (cet4List.indexOf(word) > -1) {
+			oneObj.cet4 = true;
+		}
+
+		if (cet6List.indexOf(word) > -1) {
+			oneObj.cet6 = true;
+		}
+
+		if (tofelList.indexOf(word) > -1) {
+			oneObj.tofel = true;
+		}
+
+		if (greList.indexOf(word) > -1) {
+			oneObj.gre = true;
+		}
+
+		var rank = rankList.indexOf(word);
+		if (rank > -1) {
+			oneObj.rank = rank;
+		}
 
 		let font = div.getElementsByTagName('font')[0];
 		font.parentNode.removeChild(font);
@@ -42,5 +93,5 @@ function run() {
 		}
 		allObj.push(oneObj);
 	}
-	fs.writeFileSync('result.txt', JSON.stringify(allObj, null, 4));
+	fs.writeFileSync('./output/weike.json', JSON.stringify(allObj, null, 4));
 }
